2e3abb1ac7fac7502f00786fbd96aa31d9aecb8e
Lara Nonis <lara.nonis01@gmail.com>
2025-02-09 14:27:39 +0100

    game setup, panel, bg resizers

--- battle_rpg/graphic_rpg.py
+++ battle_rpg/graphic_rpg.py
@@ -2,9 +2,13 @@ import pygame
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps= 60
+
 # pygame window
+bottom_panel = 150
 screen_width = 800
-screen_heigth = 400
+screen_heigth = 400 + bottom_panel
 
 screen = pygame.display.set_mode((screen_width, screen_heigth))
 pygame.display.set_caption('Murren final boss battle')
@@ -12,16 +16,28 @@ pygame.display.set_caption('Murren final boss battle')
 # load images
 # Game background
 background_image=pygame.image.load('img/Background/background.jpg').convert_alpha()
+# panel image
+panel_image=pygame.image.load('img/Panel/panel_double.png').convert_alpha()
+
 
 # function for drawing image
 def draw_bg():
     screen.blit(background_image, (0,0))
 
+# function to draw panel
+def draw_panel():
+    screen.blit(panel_image, (0,screen_heigth- bottom_panel))
+
+
 run = True
 while run:
 
+    clock.tick(fps)
+
     # draw background
     draw_bg()
+    # draw panel
+    draw_panel()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
