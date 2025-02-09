05f5def36f9b81ea6fdd9e8ab341a2c7a61a9862
Lara Nonis <lara.nonis01@gmail.com>
2025-02-09 20:15:34 +0100

    idle animation

--- battle_rpg/graphic_rpg.py
+++ battle_rpg/graphic_rpg.py
@@ -13,11 +13,24 @@ screen_heigth = 400 + bottom_panel
 screen = pygame.display.set_mode((screen_width, screen_heigth))
 pygame.display.set_caption('Murren final boss battle')
 
+# define font
+font= pygame.font.SysFont('Times New Roman', 26)
+
+# define color
+red=(255, 0, 0)
+green= (0, 255, 0)
+
 # load images
 # Game background
 background_image=pygame.image.load('img/Background/background.jpg').convert_alpha()
 # panel image
 panel_image=pygame.image.load('img/Icons/panel_double.png').convert_alpha()
+
+
+# create function for drawing text
+def draw_text(text, font, text_col, x, y):
+    img=font.render(text, True, text_col)
+    screen.blit(img, x, y)
 
 
 # function for drawing image
@@ -39,10 +52,40 @@ class Fighter():
         self.start_potions=potions
         self.potions=potions
         self.alive = True
-        img=pygame.image.load(f'img/{self.name}/Idle/0.png')
-        self.image=pygame.transform.scale(img, (img.get_width()*3, img.get_height()*3))
+        self.animation_list=[]
+        self.frame_index =0
+        self.action = 0 # 0: Idle , 1: attack, 2: Hurt, 3: Death
+        self.update_time=pygame.time.get_ticks()
+        # load idle images
+        temp_list=[]
+        for i in range(8):
+            img=pygame.image.load(f'img/{self.name}/Idle/{i}.png')
+            img=pygame.transform.scale(img, (img.get_width()*3, img.get_height()*3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        # load attack images
+        temp_list=[]
+        for i in range(8):
+            img=pygame.image.load(f'img/{self.name}/Attack/{i}.png')
+            img=pygame.transform.scale(img, (img.get_width()*3, img.get_height()*3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image=self.animation_list[self.action][self.frame_index]
         self.rect=self.image.get_rect()
         self.rect.center=(x,y)
+
+    def update(self):
+        animation_cooldown = 100
+        # handle animation
+        # update image
+        self.image=self.animation_list[self.action][self.frame_index]
+        # check if enough time hasd passed after pastupdate
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        # if the animation has run out then reset back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index=0
 
     def draw(self):
         screen.blit(self.image, self.rect)
@@ -68,8 +111,10 @@ while run:
     draw_panel()
 
     # draw fighters
+    knight.update()
     knight.draw()
     for bandit in bandit_list:
+        bandit.update()
         bandit.draw()
 
     for event in pygame.event.get():
